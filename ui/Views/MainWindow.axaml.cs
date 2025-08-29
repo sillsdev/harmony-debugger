823fb4abacb4222c595664c1e5b9d5f016395053
Tim Haasdyk <tim_haasdyk@sil.org>
2025-08-29 12:14:56 +0200

    sln and Avalonia project init

---
+++ ui/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HarmonyDebuggerUi.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
